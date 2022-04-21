4d95780cf72b22aca64646c672856116e40a22e9
helloye <hello.paingy@gmail.com>
2022-04-20 22:07:55 -0400

    adding frontend vanilla js listener to firebase

--- firebase_io.py
+++ firebase_io.py
@@ -1,6 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import sys
+
+# By default add 1 to count if no argument is given
+additionalCount = 1
+
+if len(sys.argv) > 1:
+	# i.e python firebase_io.py 123
+	additionalCount = int(sys.argv[1])
 
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate('./fb_secret.json')
@@ -10,9 +18,8 @@ db = firestore.client();
 
 doc_ref = db.collection("rpiCount").document('personCount');
 
-
 doc = doc_ref.get().to_dict();
-newCount = doc['currentCount'] + 1
+newCount = doc['currentCount'] + additionalCount
 
 doc_ref.set({
 	'currentCount': newCount
---
+++ index.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+	const targetElement = document.querySelector("#output");
+
+	const firebase = window.firebase;
+	const firebaseConfig = {} // Copy over config from Firebase Web SDK section.
+	firebase.initializeApp(firebaseConfig);
+
+	const db = firebase.firestore();
+	var docRef = db.collection("rpiCount").doc("personCount");
+
+	docRef.get().then(doc => {
+		targetElement.innerHTML = `${doc.data().currentCount} # of customers at the restraunt!`
+	}).catch(err => {
+		console.log("docRef get Error:", err);
+	})
+
+	db.collection("rpiCount").doc("personCount")
+    .onSnapshot((doc) => {
+    	targetElement.innerHTML = `${doc.data().currentCount} # of customers at the restraunt!`
+    });
+}
