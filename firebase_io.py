f76c6f95e547991053f50d3e7cd67c276067b646
Y Paing <ypaing@squarespace.com>
2022-04-26 14:04:03 -0400

    working sensor and firebase io

--- firebase_io.py
+++ firebase_io.py
@@ -3,24 +3,28 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import sys
 
-# By default add 1 to count if no argument is given
-additionalCount = 1
-
-if len(sys.argv) > 1:
-	# i.e python firebase_io.py 123
-	additionalCount = int(sys.argv[1])
-
-# Fetch the service account key JSON file contents
 cred = credentials.Certificate('./fb_secret.json')
 firebase_admin.initialize_app(cred)
 
-db = firestore.client();
 
-doc_ref = db.collection("rpiCount").document('personCount');
+def update_firebase(additionalCount):
+	# Fetch the service account key JSON file contents
+	
+	db = firestore.client();
 
-doc = doc_ref.get().to_dict();
-newCount = doc['currentCount'] + additionalCount
+	doc_ref = db.collection("rpiCount").document('personCount');
 
-doc_ref.set({
-	'currentCount': newCount
-})
+	doc = doc_ref.get().to_dict();
+	newCount = doc['currentCount'] + additionalCount
+
+	doc_ref.set({
+		'currentCount': newCount
+	})
+
+if __name__ == '__main__':
+	# By default add 1 to count if no argument is given
+	paramCount = 1
+	if len(sys.argv) > 1:
+		# i.e python firebase_io.py 123
+		paramCount = int(sys.argv[1])
+	update_firebase(paramCount)
--- sensor.py
+++ sensor.py
@@ -1,14 +1,69 @@
 import RPi.GPIO as GPIO
+from time import sleep
+from datetime import datetime
+import firebase_io
+
 GPIO.setmode(GPIO.BOARD)
 
 LASER_PIN_GREEN = 11
 LASER_PIN_BLUE = 12
 
+GREEN_TIME = None
+BLUE_TIME = None
+
 GPIO.setup(LASER_PIN_GREEN, GPIO.IN)
 GPIO.setup(LASER_PIN_BLUE, GPIO.IN)
 
-while True:
-	print("GPIO: Green[%2d] - Blue[%2d]" % (GPIO.input(LASER_PIN_GREEN), GPIO.input(LASER_PIN_BLUE)))
+PIN_IN_DEBOUNCE = 1
 
+GPIO.add_event_detect(LASER_PIN_GREEN, GPIO.FALLING, bouncetime=PIN_IN_DEBOUNCE)
+GPIO.add_event_detect(LASER_PIN_BLUE, GPIO.FALLING, bouncetime=PIN_IN_DEBOUNCE)
 
+customer_count = 0
+
+try:
+	while True:
+		# print("%s - %s | Count: %2d" % (GREEN_TIME, BLUE_TIME, customer_count))
+
+		if GPIO.event_detected(LASER_PIN_GREEN):
+			print("Trip Alert: GREEN - %s" % datetime.now())
+			GREEN_TIME = datetime.now()
+
+		if GPIO.event_detected(LASER_PIN_BLUE):
+			print("Trip Alert: BLUE - %s" % datetime.now())
+			BLUE_TIME = datetime.now()
+
+		if (GREEN_TIME and BLUE_TIME):
+			GPIO.remove_event_detect(LASER_PIN_GREEN)
+			GPIO.remove_event_detect(LASER_PIN_BLUE)
+
+			print("%s - %s | Current count: %2d" % (GREEN_TIME, BLUE_TIME, customer_count))
+			if (GREEN_TIME < BLUE_TIME):
+				print("Customer +1 IN")
+				customer_count+=1
+				firebase_io.update_firebase(1)
+			else:
+				print("Customer -1 OUT")
+				customer_count-=1
+				firebase_io.update_firebase(-1)
+
+			sleep(1.75)
+
+			GREEN_TIME = None
+			BLUE_TIME = None
+
+			print("Resetting timer (should be None): %s - %s | Current count: %2d" % (GREEN_TIME, BLUE_TIME, customer_count))
+
+			GPIO.add_event_detect(LASER_PIN_GREEN, GPIO.FALLING, bouncetime=PIN_IN_DEBOUNCE)
+			GPIO.add_event_detect(LASER_PIN_BLUE, GPIO.FALLING, bouncetime=PIN_IN_DEBOUNCE)
+
+			
+
+		# sleep(.3)
+except KeyboardInterrupt:
+    pass
+
+print("Total customer count: ", customer_count)
+
+GPIO.cleanup()
 print ('Hello World!')
